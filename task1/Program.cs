a4d0ce6475fbd5db0d8c251d6d3bd24e4b68d87b
Juli-ia <julie78sd@gmail.com>
2018-05-17 11:17:19 +0300

    test1

--- task1/Program.cs
+++ task1/Program.cs
@@ -10,6 +10,27 @@ namespace task1
     {
         static void Main(string[] args)
         {
+            AbstractHandler xml = new XMLHandler();
+            xml.Create();
+            xml.Open();
+            xml.Change();
+            xml.Save();
+            Console.WriteLine();
+
+            AbstractHandler txt = new TXTHandler();
+            txt.Create();
+            txt.Open();
+            txt.Change();
+            txt.Save();
+            Console.WriteLine();
+
+            AbstractHandler doc = new DOCHandler();
+            doc.Create();
+            doc.Open();
+            doc.Change();
+            doc.Save();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ task1/XMLHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class XMLHandler : AbstractHandler
+    {
+        public string type = "XML";
+
+        public override void Open()
+        {
+            Console.WriteLine("{0} document has been opened.", type);
+        }
+        public override void Create()
+        {
+            Console.WriteLine("{0} document has been created.", type);
+        }
+        public override void Change()
+        {
+            Console.WriteLine("{0} document has been changed.", type);
+        }
+        public override void Save()
+        {
+            Console.WriteLine("{0} document has been saved.", type);
+        }
+    }
+}
